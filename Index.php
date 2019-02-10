26cae161919bfa134fb0fb8693b9e13c55228aa1
Thiago Santos <thiiago.saantos86@gmail.com>
2019-02-10 17:37:39 -0200

    Mudanças da Tela de Login e Cadastro

--- Index.php
+++ Index.php
@@ -6,10 +6,11 @@
 
   <link rel="icon" href="Imagens/favi.ico" type="image/ico" sizes="64x64">
   <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-   <link rel="stylesheet" type="text/css" href="CSS/styleIndex.css">
+  <link rel="stylesheet" type="text/css" href="CSS/styleIndex.css">
 </head>
 
 <body>
+
   <div id="login-box">
 
     <div id="login-logo">
@@ -19,11 +20,11 @@
     <div class="Container">
         <form method="POST" class="form" action="Controle/entra.php">
           <div class="form-group">
-            <label>Nome de Usuário: <input name="nomeUsuario" type="text" required placeholder="Login" class="form-control"/></label>
+            <label>Matrícula: <input name="nomeUsuario" type="text" required placeholder="Login" class="form-control"/></label>
           </div>
-          <div class="form-group" style="font-size:13px;">
+          <div class="form-group">
             <label>Senha: <input name="senha" type="password" required minlength="6" maxlength="12" placeholder="******" class="form-control"/></label>
-          </br>  <a href="cadastro.php" name="entrar" style="font-size:13px;">Cadastre-se</a>
+          </br>  <a href="cadastro.php" name="entrar">Cadastre-se</a>
           </div>
 
 
